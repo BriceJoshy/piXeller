92c0e46b143c781c157007df008eeba1a7a2e827
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-04 18:51:08 +0530

    model for onboarding screen

---
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class onBoardingScreen extends StatelessWidget {
+  const onBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

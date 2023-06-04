62aa7d0091a453a3be157c8570faad9426d63aac
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-04 16:12:32 +0530

    theme app data

--- lib/src/utils/themes/theme.dart
+++ lib/src/utils/themes/theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mini_project_1/src/utils/themes/widget_themes/text_theme.dart';
 
 class MyAppTheme {

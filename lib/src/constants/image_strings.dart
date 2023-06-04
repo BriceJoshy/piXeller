6eece693e9d707e71867d20d64868a292f5879da
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-04 18:22:40 +0530

    image_strings

---
+++ lib/src/constants/image_strings.dart
@@ -0,0 +1,12 @@
+// ---Splash Screen images---
+
+// const String mySplashTopIcon =
+// const String mySplashImage =
+
+// ---ONBOARDING SCREEN IMAGES---
+const String myOnboardingImage1 =
+    "assets/images/onboarding_screen/onboarding_1.gif";
+const String myOnboardingImage2 =
+    "assets/images/onboarding_screen/onboarding_2.gif";
+const String myOnboardingImage3 =
+    "assets/images/onboarding_screen/onboarding_3.gif";

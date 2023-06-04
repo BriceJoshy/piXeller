5b7b84fe323e1db41853e2a0ba51c4730df51c79
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-04 15:58:01 +0530

    app and text themes added

--- lib/src/utils/themes/theme.dart
+++ lib/src/utils/themes/theme.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mini_project_1/src/utils/themes/widget_themes/text_theme.dart';
 
 class MyAppTheme {
-  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+  // we don't want the user to access the theme using the instance of the class so we have to make the constructor private
+  MyAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light, textTheme: MyTextTheme.lightTextTheme);
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark, textTheme: MyTextTheme.darkTextTheme);
 }

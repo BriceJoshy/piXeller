63a3847c33396143043017e2e71be67836edfbba
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-04 18:22:30 +0530

    colors

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// LIST OF ALL COLORS
+const myPrimaryColor = Color(0xffffe400);
+const mySecondaryColor = Color(0xff272727);
+const myaccentColor = Color(0xff001bff);
+
+const myWhiteColor = Colors.white;
+const myDarkColor = Colors.black;
+const myCardBgColor = Color(0xfff7f6f1);
+
+// Onbourding Colors
+
+const myOnboardingPage1Color = Colors.white;
+const myOnboardingPage2Color = Color(0xfffddcdf);
+const myOnboardingPage3Color = Color(0xffffdcbd);

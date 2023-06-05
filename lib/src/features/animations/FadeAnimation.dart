fc85527c05cae16bccb72ebacd10b62c8e5dad2f
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-05 23:54:40 +0530

    fade animation

--- lib/src/features/animations/FadeAnimation.dart
+++ lib/src/features/animations/FadeAnimation.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:simple_animations/simple_animations.dart';
 
 class FadeAnimation extends StatelessWidget {
   final double delay;
   final Widget child;
 
-  FadeAnimation(this.delay, this.child);
+  const FadeAnimation(this.delay, this.child, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    final tween = MultiTrackTween([
-      Track("opacity").add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
-      Track("translateY").add(
-        Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
-        curve: Curves.easeOut)
-    ]);
+    const animationDuration = Duration(milliseconds: 500);
 
-    return ControlledAnimation(
-      delay: Duration(milliseconds: (500 * delay).round()),
-      duration: tween.duration,
-      tween: tween,
-      child: child,
-      builderWithChild: (context, child, animation) => Opacity(
-        opacity: animation["opacity"],
-        child: Transform.translate(
-          offset: Offset(0, animation["translateY"]), 
-          child: child
-        ),
-      ),
+    return FutureBuilder(
+      future: Future.delayed(Duration(milliseconds: (500 * delay).round())),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox.shrink(); // Placeholder widget during the delay
+        } else {
+          return AnimatedOpacity(
+            opacity: 1.0,
+            duration: animationDuration,
+            curve: Curves.easeOut,
+            child: child,
+          );
+        }
+      },
     );
   }
 }

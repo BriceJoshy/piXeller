8c2b908e98a3fb05f62d7a51c121f95a42062419
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-05 23:32:10 +0530

    login screen images strings

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -10,3 +10,11 @@ const String myOnboardingImage2 =
     "assets/images/onboarding_screen/onboarding_2.png";
 const String myOnboardingImage3 =
     "assets/images/onboarding_screen/onboarding_3.png";
+
+// --LLOGIN SCREEN IMAGES--
+
+const String myLoginPageBackground =
+    "assets/images/login_screen/background.png";
+const String myLoginPageLight1 = "assets/images/login_screen/light_1.png";
+const String myLoginPageLight2 = "assets/images/login_screen/light_2.png";
+const String myLoginPageClock = "assets/images/login_screen/clock.png";

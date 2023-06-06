cfd954ecb8b4d8a2cf6a4f8dca4308edc82df0cf
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-06 18:56:35 +0530

    images added for wecome page

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -18,3 +18,7 @@ const String myLoginPageBackground =
 const String myLoginPageLight1 = "assets/images/login_screen/light_1.png";
 const String myLoginPageLight2 = "assets/images/login_screen/light_2.png";
 const String myLoginPageClock = "assets/images/login_screen/clock.png";
+
+// --WELCOME PAGE IMAGES--
+const String myWelcomePageShapesAnimation = "assets/RiveAssets/shapes.riv";
+const String myWelcomePageSpline = "assets/images/welcome_page/Spline.png";
--- lib/src/features/authentication/screens/welcome_screen/welcome_screen.dart
+++ lib/src/features/authentication/screens/welcome_screen/welcome_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:mini_project_1/src/constants/image_strings.dart';
 import 'package:rive/rive.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -8,7 +11,28 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [/*RiveAnimation.asset("assets/RiveAssets/shapes.riv")*/],
+        children: [
+          Positioned(
+            bottom: 200,
+            left: 100,
+            // height: 100,
+            width: MediaQuery.of(context).size.width * 1.7,
+            child: Image.asset(myWelcomePageSpline),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+              child: const SizedBox(),
+            ),
+          ),
+          const RiveAnimation.asset(myWelcomePageShapesAnimation),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+              child: const SizedBox(),
+            ),
+          ),
+        ],
       ),
     );
   }

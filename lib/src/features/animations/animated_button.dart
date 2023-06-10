041204112fa0cb4d4f0073a72c1158743c0a7f98
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-10 22:31:03 +0530

    adding the login and sign in gifs for the selectn

--- lib/src/features/animations/animated_button.dart
+++ lib/src/features/animations/animated_button.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_button/flutter_animated_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mini_project_1/src/features/authentication/screens/login_page/login_page%20copy.dart';
 import 'package:mini_project_1/src/features/authentication/screens/login_page/login_page.dart';
 
@@ -13,13 +15,62 @@ class MyAnimatedButton extends StatelessWidget {
       width: 200,
       borderRadius: 20,
       onPress: () {
-        // Future.delayed(
-        //   const Duration(milliseconds: 500),
-        //   () {
-        //     Navigator.push(context,
-        //         MaterialPageRoute(builder: (_) => const LoginScreenTest()));
-        //   },
-        // );
+        Future.delayed(
+          const Duration(milliseconds: 300),
+          () {
+            showGeneralDialog(
+              barrierDismissible: true,
+              barrierLabel: "Login Or Sign In",
+              context: context,
+              pageBuilder: (context, _, __) => Center(
+                child: Container(
+                  height: 500,
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                  ),
+                  child: Scaffold(
+                    backgroundColor: Colors.transparent,
+                    body: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            color: Colors.orange,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 30),
+                            child: Text(
+                              "Connecting Producers and Distributors for Greater Opportunities and Increased Income!",
+                              style: GoogleFonts.poppins(),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xfff77d8e),
+                                minimumSize: Size(double.infinity, 56)),
+                            onPressed: () {},
+                            icon: Image.asset("name"),
+                            label: Text(
+                              "Login",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 34, fontWeight: FontWeight.w700),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
       },
       text: 'Join Now',
       selectedTextColor: Colors.black,

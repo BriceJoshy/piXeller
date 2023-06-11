0162b4d728db1e0e3422b3861ceb7c8c8c133cce
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-11 21:38:50 +0530

    firebase widgets ensure initialized

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'src/features/authentication/screens/on_boarding/on_boarding_screen.dart'
 var size, height, width;
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );

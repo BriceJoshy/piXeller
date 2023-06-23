5813b2a1419ce16c2234349bb2073a6753498039
BRICE JOSHY <bricejoshy99@gmail.com>
2023-06-23 23:12:24 +0530

    home screen

--- lib/src/features/authentication/screens/homescreen/homescreen.dart
+++ lib/src/features/authentication/screens/homescreen/homescreen.dart
@@ -1,76 +1,161 @@
 import 'package:flutter/material.dart';
-import 'package:mini_project_1/src/common_widgets/grid_container.dart';
-import 'package:mini_project_1/src/common_widgets/tabbar_material_widget.dart';
-import 'package:mini_project_1/src/constants/image_strings.dart';
-import 'package:mini_project_1/src/features/authentication/screens/on_boarding/on_boarding_screen.dart';
-import 'package:rive/rive.dart';
+import 'package:mini_project_1/src/features/authentication/screens/sideBarScreen/next_page.dart';
 
-import '../../../../apis/api.dart';
-import '../login_page/animated_login_page.dart';
+import '../../../../constants/image_strings.dart';
 
 class homeScreen extends StatefulWidget {
-  const homeScreen({super.key});
+  const homeScreen({Key? key}) : super(key: key);
 
   @override
-  State<homeScreen> createState() => _homeScreenState();
+  _homeScreenState createState() => _homeScreenState();
 }
 
 class _homeScreenState extends State<homeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar: const tabbar_material_widget(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            APIs.auth.signOut().then((value) => Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (_) => onBoardingScreen())));
-          },
-          child: const Icon(Icons.add),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
+      body: Stack(
+        children: [
+          const NextPage(),
+          // homescreen page stuff
+          MySideBarButton(
+            press: () {},
+          )
+        ],
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+          decoration: const BoxDecoration(
+            color: Color(0xff3a4054),
+            borderRadius: BorderRadius.all(
+              Radius.circular(24),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MyBottomNavigationBarIcons(
+                image: Image.asset(
+                  myHomePageBottomHomeIcon,
+                  color: _selectedIndex == 0 ? Colors.white : Colors.grey,
+                ),
+                onTap: () => _onItemTapped(0),
+              ),
+              MyBottomNavigationBarIcons(
+                image: Image.asset(
+                  myHomePageBottomHistoryIcon,
+                  color: _selectedIndex == 1 ? Colors.white : Colors.grey,
+                ),
+                onTap: () => _onItemTapped(1),
+              ),
+              _buildNotchedButton(),
+              MyBottomNavigationBarIcons(
+                image: Image.asset(
+                  myHomePageBottomAccountIcon,
+                  color: _selectedIndex == 3 ? Colors.white : Colors.grey,
+                ),
+                onTap: () => _onItemTapped(3),
+              ),
+              MyBottomNavigationBarIcons(
+                image: Image.asset(
+                  myHomePageBottomSettingsIcon,
+                  color: _selectedIndex == 4 ? Colors.white : Colors.grey,
+                ),
+                onTap: () => _onItemTapped(4),
+              ),
+            ],
+          ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        body: GridView.count(
-          crossAxisCount: 3,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8,
-          padding: EdgeInsets.all(10),
-          children: [
-            grid_container(
-              name: "Tomato",
-              color: Colors.grey,
-              image: Image.asset("assets/icons/role.png"),
-            ),
-            grid_container(
-              name: "Tomato",
-              color: Colors.grey,
-              image: Image.asset("assets/icons/role.png"),
-            ),
-            grid_container(
-              name: "Tomato",
-              color: Colors.grey,
-              image: Image.asset("assets/icons/role.png"),
-            ),
-            grid_container(
-              name: "Tomato",
-              color: Colors.grey,
-              image: Image.asset("assets/icons/role.png"),
-            )
-          ],
+      ),
+    );
+  }
+
+  Widget _buildNotchedButton() {
+    return SizedBox(
+      height: 47,
+      width: 47,
+      child: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.add,
+          color: Colors.blue,
         ),
       ),
     );
   }
 }
 
+class MySideBarButton extends StatelessWidget {
+  const MySideBarButton({
+    super.key,
+    required this.press,
+  });
+  final VoidCallback press;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: GestureDetector(
+        onTap: press,
+        child: Container(
+          margin: const EdgeInsets.only(left: 20, top: 30),
+          padding: const EdgeInsetsDirectional.all(10),
+          height: 40,
+          width: 40,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, 3),
+                blurRadius: 8,
+              ),
+            ],
+          ),
+          child: Image.asset(
+            myHomePageBottomSideBarIcon,
+            height: 32,
+            width: 32,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-//  Center(
-//         child: TextButton(
-//           onPressed: () => {
-//             APIs.auth.signOut().then((value) => Navigator.pushReplacement(
-//                 context,
-//                 MaterialPageRoute(builder: (_) => AnimatedLoginForm())))
-//           },
-//           child: Text("Logout"),
-//         ),
-//       ),
+class MyBottomNavigationBarIcons extends StatelessWidget {
+  final Image image;
+  final VoidCallback onTap;
+
+  const MyBottomNavigationBarIcons({
+    Key? key,
+    required this.image,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 32,
+      width: 32,
+      child: GestureDetector(
+        onTap: onTap,
+        child: image,
+      ),
+    );
+  }
+}

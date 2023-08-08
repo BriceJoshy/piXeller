587a48ec83ad020008ee7e4a4b8f16fd7f043ec0
BRICE JOSHY <bricejoshy99@gmail.com>
2023-08-08 14:05:01 +0530

    distrubuter dcreen

--- lib/src/features/core_Screens/DistributerScreen/distibuter_homeDrawerScreen.dart
+++ lib/src/features/core_Screens/DistributerScreen/distibuter_homeDrawerScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mini_project_1/src/components/SIdeBarStuff/drawerScreen.dart';
 import 'package:mini_project_1/src/features/core_Screens/DistributerScreen/Distributer_HOmeBody.dart';
 import 'package:mini_project_1/src/features/core_Screens/DistributerScreen/DistributerdrawerScreen.dart';
 

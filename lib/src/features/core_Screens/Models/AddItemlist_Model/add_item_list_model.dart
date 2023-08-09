ebe8cb60ceeb3cc40b8252ee9e297fed7ebb4b5f
BRICE JOSHY <bricejoshy99@gmail.com>
2023-08-09 09:00:52 +0530

    bid item model design

--- lib/src/features/core_Screens/Models/AddItemlist_Model/add_item_list_model.dart
+++ lib/src/features/core_Screens/Models/AddItemlist_Model/add_item_list_model.dart
@@ -56,3 +56,62 @@ class AddItemListModel {
     return data;
   }
 }
+
+class BidAddItemListModel {
+  BidAddItemListModel(
+      {required this.id,
+      required this.itemCategory,
+      required this.itemName,
+      required this.itemQuantity,
+      required this.itemPrice,
+      required this.producerNumber,
+      required this.image,
+      required this.producerWarning,
+      required this.itemDescription,
+      required this.createdAt,
+      required this.itemAdded});
+  late String id;
+  late String itemCategory;
+  late String itemName;
+  late String itemQuantity;
+  late int itemPrice;
+  late String producerNumber;
+  late String image;
+  late String producerWarning;
+  late String itemDescription;
+  late String createdAt;
+  late bool itemAdded;
+
+  BidAddItemListModel.fromJson(Map<String, dynamic> json) {
+    // if the value is null that is if not filled then return empty String ?? ''
+
+    id = json['id'] ?? '';
+    itemCategory = json['itemCategory'] ?? '';
+    itemName = json['itemName'] ?? '';
+    itemQuantity = json['itemQuantity'] ?? '';
+    itemPrice = json['itemPrice'] ?? '';
+    producerNumber = json['producerNumber'] ?? '';
+    image = json['image'] ?? '';
+    producerWarning = json['producerWarning'] ?? '';
+    itemDescription = json['itemDescription'] ?? '';
+    itemAdded = json['itemAdded'] ?? '';
+    createdAt = json['created_at'] ?? '';
+  }
+
+  Map<String, dynamic> toJson() {
+    // no need to maje data private "_data"
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['itemCategory'] = itemCategory;
+    data['itemName'] = itemName;
+    data['itemQuantity'] = itemQuantity;
+    data['itemPrice'] = itemPrice;
+    data['producerNumber'] = producerNumber;
+    data['image'] = image;
+    data['producerWarning'] = producerWarning;
+    data['itemDescription'] = itemDescription;
+    data['itemAdded'] = itemAdded;
+    data['created_at'] = createdAt;
+    return data;
+  }
+}
